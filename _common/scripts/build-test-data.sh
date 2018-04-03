883eef2f400df809e0463ae2937a60087915b135
Jeremy Sells <jeremy.sells@pulsant.com>
2018-04-03 11:40:10 +0100

    Creating master branch

--- _common/scripts/build-test-data.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-#docker run -v ./var/certs:/certs ubuntu:16.04
-
-docker run --rm -v ${PWD}/var/dhparam:/dhparam ubuntu:16.04 sh -c 'apt update; apt install -y openssl; openssl dhparam -out /dhparam/dhparam.pem 2048'
-
-docker run --rm -v ${PWD}/var/certs:/certs -v ${PWD}/bin/development/create_self_signed_certs.sh:/create_self_signed_certs.sh ubuntu:16.04 sh -c 'apt update; apt install -y openssl; /create_self_signed_certs.sh -f asset-engine -p  /certs'
